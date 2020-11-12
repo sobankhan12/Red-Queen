a23b0f2171a985e2670590668e268b326a5dccc4
Soban khan <sobankhan252@gmail.com>
2020-11-12 13:47:39 +0500

    Update App.js
    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-// const { Red1 } = require("./Red1");
-// const { default: Red2 } = require("./Red2");
-
-const { default: RedQueen } = require("./Red2");
-
-
+import RedQueen from "./RedQueen";
 function App() {
   return (
     <div className="App">
-{/* <Red2></Red2> */}
-{/* <Red1></Red1> */}
+
 <RedQueen></RedQueen>
     </div>
   );
